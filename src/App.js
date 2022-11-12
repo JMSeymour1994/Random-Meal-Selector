9fa1465e747a3bae7533ec86adeba29badafa1bd
JMSeymour1994 <j.seymour1994@gmail.com>
2022-11-12 11:30:14 +0000

    test commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Test
+          Random Meal Selector
         </a>
       </header>
     </div>
