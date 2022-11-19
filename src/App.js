662c3d628c74f26e7cd8f540e014cab3caf29644
JMSeymour1994 <j.seymour1994@gmail.com>
2022-11-19 11:48:05 +0000

    191122

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
 // import * as ReactBootstrap from 'react-bootstrap';
-import {NumOfMeals} from './helperFunctions';
-// import mealRandomiser from './mealRandomiser';
+import {MealRandomiser} from './mealRandomiser';
 
 function App() {
   return (
@@ -22,23 +21,12 @@ function App() {
       <section id='parameters' className='px-5 pb-5'>
         <div className='container'>
           <div className='row'>
-            <div className='d-flex col-7 justify-content-end pb-4'>
-              <p>Select the number of meals:</p>
-            </div>
-            <div className="num-of-meals-dropdown col-5">
-              <NumOfMeals/>
-            </div>
-          </div>
-          <div className='row'>
-            <div className='d-flex col-7 justify-content-end pb-4'>
-              <p>Output:</p>
-            </div>
-            <div className="meals-output col-5">
+            <div className="meal-randomiser col">
+              <MealRandomiser/>
             </div>
           </div>
         </div>
       </section>
-
     </div>
   );
 }
--- src/mealRandomiser.js
+++ src/mealRandomiser.js
@@ -1,38 +1,67 @@
+import React, { useReducer } from "react";
+import * as ReactBootstrap from 'react-bootstrap';
 
-const listOfMeals = [
-    "Pizza",
-    "Stir fry",
-    "Roast",
-    "Take away",
-    "Chilli",
-    "Mushroom pasta",
-    "Lasagne",
-    "Fish and chips",
-    "Halloumi burgers",
-    "Fishcakes",
-    "Mozzarella Burgers"
+const mealsArr = ["Pizza", "Stir fry",
+"Roast", "Take away",
+"Chilli", "Mushroom pasta",
+"Lasagne", "Fish and chips",
+"Halloumi burgers", "Fishcakes",
+"Mozzarella Burgers"
 ];
 
-const numOfDays = 7;
-
-/**
- * This function takes in an array of meal choices and returns 
- * a random selection equal to the number of days input
- * @param {array} listOfMeals 
- * @param {number} numOfDays 
- * @returns {array} 
- */
-function mealRandomiser(listOfMeals, numOfDays) {
-
-    function shuffle() {
-    for (let i = listOfMeals.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [listOfMeals[i], listOfMeals[j]] = [listOfMeals[j], listOfMeals[i]];
-    }
-    return listOfMeals.slice(0, numOfDays);
-    }
-
-    return shuffle();
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return {...state, numOfMeals: state.numOfMeals + 1}
+    case "decrement":
+      return {...state, numOfMeals: state.numOfMeals - 1}
+    case "meals":
+      return {...state, meals: action.payload};
+    default:
+      throw new Error();
+  }
 }
 
-export default mealRandomiser(listOfMeals, numOfDays);
+export function MealRandomiser() {
+
+  // Declare the meal state variables
+  const [state, dispatch] = useReducer(reducer, 
+    {numOfMeals: 7, meals: mealsArr.slice(0, 7)});
+
+  function shuffle() {
+    for (let i = mealsArr.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [mealsArr[i], mealsArr[j]] = [mealsArr[j], mealsArr[i]];
+    }
+    return mealsArr.slice(0, state.numOfMeals);
+  }
+
+  return (
+    <div className="App">
+      <div className="container">
+        <section id="buttons">
+            <ReactBootstrap.InputGroup.Text>
+                <div className="row">
+                  <div className="col-7">
+                    Number of meals: {state.numOfMeals}
+                  </div>
+                  <div className="col-5">
+                    <ReactBootstrap.Button className="me-1" onClick={() => dispatch({ type: "decrement"})}>-</ReactBootstrap.Button>
+                    <ReactBootstrap.Button className="me-1" onClick={() => dispatch({ type: "increment"})}>+</ReactBootstrap.Button>
+                    <ReactBootstrap.Button className="me-5" onClick={() => dispatch({type: "meals", payload: shuffle()})}>Enter</ReactBootstrap.Button>
+                  </div>
+                </div>
+              </ReactBootstrap.InputGroup.Text>
+        </section>
+        <section id="list" className="py-4">
+            <ReactBootstrap.ListGroup as="ol" numbered>
+            {state.meals.map((item) => {
+              return <ReactBootstrap.ListGroup.Item as="li">{item}</ReactBootstrap.ListGroup.Item>
+            })}
+            </ReactBootstrap.ListGroup>
+        </section>
+      </div>
+    </div>
+  )
+}
+
